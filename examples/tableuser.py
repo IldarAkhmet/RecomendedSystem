687ab7b68baceeb343f8ec4eb0de9b506c63d3bd
IldarAkhm <112767933+IldarAkhm@users.noreply.github.com>
2023-09-15 16:31:08 +0300

    realizated_tableuser~

---
+++ examples/tableuser.py
@@ -0,0 +1,17 @@
+from database import Base
+from sqlalchemy.types import Integer, String
+from sqlalchemy import Column
+
+
+class User(Base):
+    __tablename__ = 'user'
+    __table_args__ = {'schema': 'public'}
+
+    age = Column(Integer)
+    city = Column(String)
+    country = Column(String)
+    exp_group = Column(Integer)
+    gender = Column(Integer)
+    id = Column(Integer, primary_key=True)
+    os = Column(String)
+    source = Column(String)
