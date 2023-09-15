a5baee39a970629ffa1e27477105b0e958422a47
IldarAkhm <112767933+IldarAkhm@users.noreply.github.com>
2023-09-16 02:10:13 +0300

    realizated_schema

---
+++ examples/schema.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class UserGet(BaseModel):
+    id: int
+    age: int
+    city: str
+    country: str
+    exp_group: int
+    gender: int
+    os: str
+    source: str
+
+    class Config:
+        orm_mode = True
+
+
+class PostGet(BaseModel):
+    id: int
+    text: str
+    topic: str
+
+    class Config:
+        orm_mode = True
+
+
+class FeedGet(BaseModel):
+    action: str
+    post_id: int
+    time: datetime
+    user_id: int
+    user: UserGet
+    post: PostGet
+
+    class Config:
+        orm_mode = True
