b2e5658b72c2079cf593234d094f2394fec3c68a
IldarAkhmet <145066478+IldarAkhmet@users.noreply.github.com>
2024-02-12 14:43:17 +0300

    Update database.py

--- examples/database.py
+++ examples/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://robot-startml-ro:pheiph0hahj1Vaif@postgres.lab.karpov.courses:6432/startml"
+SQLALCHEMY_DATABASE_URL = "path_to_database"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL) # создаем движок для подключения к базе данных
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) # фабрика сессий для работы с бд
